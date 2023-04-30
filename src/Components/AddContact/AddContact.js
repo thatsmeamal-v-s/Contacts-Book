ef8ac2a961fa1e67ff1c0323e81ef34de15229a3
thatsmeamal-v-s <thatsmeamal@gmail.com>
2023-04-30 21:09:47 +0530

    replaced ref with useState in add contact functionality

--- src/Components/AddContact/AddContact.js
+++ src/Components/AddContact/AddContact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddContact.css";
 import Button from "../../Elements/Button/Button";
 import { useNavigate } from "react-router-dom";
@@ -9,21 +9,24 @@ const AddContact = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  let firstName = React.createRef();
-  let lastName = React.createRef();
-  let active = React.createRef();
-  let inactive = React.createRef();
+  let [firstName, setFirstName] = useState("");
+  let [lastName, setLastName] = useState("");
+  let [status, setStatus] = useState("active");
+
+  const handleChange = (key, e) => {
+    if (key === "firstName") {
+      setFirstName(e.target.value);
+    } else if (key === "lastName") {
+      setLastName(e.target.value);
+    } else {
+      setStatus(key);
+    }
+  };
 
   const saveContact = () => {
-    let first = firstName.current.value;
-    let last = lastName.current.value;
-    let status = "active";
-    if (inactive.current.checked) {
-      status = inactive.current.value;
-    }
-    if (first && last) {
+    if (firstName && lastName) {
       dispatch(
-        addContact({ firstName: first, lastName: last, status: status })
+        addContact({ firstName: firstName, lastName: lastName, status: status })
       );
     } else {
       alert("One or more values missing");
@@ -35,24 +38,42 @@ const AddContact = () => {
     <div className="add-contact-container">
       <div>
         <label>First Name: </label>
-        <input type="text" placeholder="First Name" ref={firstName} />
+        <input
+          key="firstName"
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => handleChange("firstName", e)}
+        />
       </div>
       <div>
         <label>Last Name: </label>
-        <input type="text" placeholder="Last Name" ref={lastName} />
+        <input
+          key="lastName"
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => handleChange("lastName", e)}
+        />
       </div>
       <div className="status-container">
         <label>Status:</label>
         <div className="status">
-          <input type="radio" value="active" name="radio" ref={active} />
+          <input
+            type="radio"
+            value="active"
+            name="radio"
+            checked={status === "active"}
+            onChange={(e) => handleChange("active", e)}
+          />
           <span>Active</span>
           <br></br>
           <input
             type="radio"
             name="radio"
             value="inactive"
-            ref={inactive}
-            checked={true}
+            checked={status === "inactive"}
+            onChange={(e) => handleChange("inactive", e)}
           />
           <span>Inactive</span>
         </div>
